a628d43d7b5ac2eda33e2eb9614f8ec2d95a0037
lionelsosa <lsosaestrada@my.apsu.edu>
2020-03-28 02:19:50 -0500

    Completed second json parsing and added results to the recipe.

--- app/src/main/java/edu/apsu/csci/nutritionnow/MainActivity.java
+++ app/src/main/java/edu/apsu/csci/nutritionnow/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.apsu.csci.nutritionnow;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,8 +33,8 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
-    List<foodItem> items = new ArrayList<>();
-    List<foodItem> recipeItems = new ArrayList<>();
+    List<FoodItem> items = new ArrayList<>();
+    List<FoodItemExtended> recipeItems = new ArrayList<>();
     String search_url;
 
     @Override
@@ -43,28 +46,70 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
-    private SearchResultsDownload downloadResults;
+    private SearchResultsDownload downloadResults; //first API
+    private RecipeItemsDownload downloadRecipeItem; //second API
 
-    public void searchButtonListener(View v){ //only button in activity
-        //clear data
-        items.clear();
+    public void buttonListener(View v) { //only button in activity
+        if (v == findViewById(R.id.search_button)) {
+            //clear data
+            items.clear();
 
-        // get search query
-        EditText searchText = findViewById(R.id.search_text);
+            // get search query
+            EditText searchText = findViewById(R.id.search_text);
 
-        // build url
-        Uri.Builder builder = Uri.parse("https://api.nal.usda.gov/fdc/v1/search").buildUpon();
-        builder.appendQueryParameter("api_key", getResources().getString(R.string.api_key));
-        builder.appendQueryParameter("generalSearchInput", searchText.getText().toString());
-        search_url = builder.toString();
-        downloadResults = new SearchResultsDownload();
-        downloadResults.execute();
+            // build url
+            Uri.Builder builder = Uri.parse("https://api.nal.usda.gov/fdc/v1/search").buildUpon();
+            builder.appendQueryParameter("api_key", getResources().getString(R.string.api_key));
+            builder.appendQueryParameter("generalSearchInput", searchText.getText().toString());
+            builder.appendQueryParameter("includeDataTypeList", "Survey (FNDDS)");
+            search_url = builder.toString();
+            downloadResults = new SearchResultsDownload();
+            downloadResults.execute();
+        }
+
+        if (v == findViewById(R.id.go_recipe_button)) {
+            //switch activity here
+        }
+
     }
-
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        recipeItems.add(items.get(position));
+
+        final int pos = position;
+        // Build an AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Confirm your choice");
+        builder.setMessage("Add " + items.get(position).description + "(ID " + items.get(position).id + ") to the recipe?");
+
+        // Set the alert dialog yes button click listener
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //parse extra fields from second API
+                // build url
+                Uri.Builder builder = Uri.parse("https://api.nal.usda.gov/fdc/v1/" + items.get(pos).id).buildUpon();
+                builder.appendQueryParameter("api_key", getResources().getString(R.string.api_key));
+                search_url = builder.toString();
+                downloadRecipeItem = new RecipeItemsDownload();
+                downloadRecipeItem.execute();
+
+
+            }
+        });
+
+        // Set the alert dialog no button click listener
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Toast.makeText(getApplicationContext(),
+                //        "Food was not added",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        // Display the alert dialog on interface
+        dialog.show();
     }
 
     private class SearchResultsDownload extends AsyncTask<Void, Void, List> {
@@ -75,7 +120,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             try {
                 URL url = new URL(search_url);
                 HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-
                 InputStream is = connection.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
                 BufferedReader br = new BufferedReader(isr);
@@ -100,19 +144,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                     //parse values
                     String foodDescription = item.getString("description");
-                    String foodBrand = "";
-                    if (item.has("brandOwner"))
-                        foodBrand = item.getString("brandOwner");
-                    else
-                        foodBrand = null;
                     int foodId = item.getInt("fdcId");
 
                     //create new item
-                    foodItem currentItem = new foodItem(foodDescription, foodBrand, foodId);
+                    FoodItem currentItem = new FoodItem(foodDescription, foodId);
                     items.add(currentItem);
                 }
                 connection.disconnect();
-
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -128,10 +166,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         protected void onPostExecute(List item) {
             String[] results = new String[items.size()];
             for (int i = 0; i < items.size(); i++ ){
-                if (items.get(i).brand != null)
-                    results[i] = items.get(i).description + " by " + items.get(i).brand;
-                else
-                    results[i] = items.get(i).description + ", generic or unbranded";
+                    results[i] = items.get(i).description;
             }
             ListView listView = (ListView) findViewById(R.id.results_View);
             ArrayAdapter<String> aa = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, results);
@@ -139,16 +174,146 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
     }
 
-    private class foodItem {
+    private class RecipeItemsDownload extends AsyncTask<Void, Void, List> {
+
+        @Override
+        protected List doInBackground(Void... voids) {
+
+            try {
+
+                int cals=0;
+                String portionUnits = "";
+                int portionSize = 0;
+
+                URL url = new URL(search_url);
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+
+                StringBuilder jsonData = new StringBuilder();
+                String line;
+                //Read the data
+                while ((line = br.readLine()) != null) {
+                    jsonData.append(line);
+                }
+
+
+                //Parse root object
+                JSONObject rootObject = new JSONObject(jsonData.toString());
+                //Parse nutrients
+                JSONArray nutrients = rootObject.getJSONArray("foodNutrients");
+                String description = rootObject.getString("description");
+
+                JSONArray nutrientInputs = rootObject.getJSONArray("inputFoods");
+                JSONObject nutrientInputFirst = nutrientInputs.getJSONObject(0);
+                portionSize = nutrientInputFirst.getInt("amount");
+                portionUnits = nutrientInputFirst.getString("unit");
+
+
+                //parse individual results
+                for (int i = 0; i < nutrients.length(); i++) {
+                    JSONObject nutrient = nutrients.getJSONObject(i);
+                    JSONObject nutrientData = nutrient.getJSONObject("nutrient");
+
+                    //parse values
+                    String nutrientName = nutrientData.getString("name");
+                    String nutrientUnits = nutrientData.getString("unitName");
+
+                    int nutrientAmount = nutrient.getInt("amount");
+
+
+                    //assign value to nutrients wanted
+                    switch (nutrientName){
+                        case "Energy":
+                            cals = nutrientAmount;
+                            break;
+                    }
+                }
+                recipeItems.add(new FoodItemExtended(new FoodItem(description, 0), cals, portionSize, portionUnits));
+
+                connection.disconnect();
+
+            } catch (MalformedURLException e) {
+                Log.e("URL", e.toString());
+            } catch (IOException e) {
+                Log.e("IO", e.toString());
+            } catch (JSONException e) {
+                Log.e("JSON", e.toString());
+            }
+
+            return items;
+        }
+
+        @Override
+        protected void onPostExecute(List item) {
+         /*   String[] results = new String[items.size()];
+            for (int i = 0; i < items.size(); i++ ){
+                results[i] = items.get(i).description;
+            }
+            ListView listView = (ListView) findViewById(R.id.results_View);
+            ArrayAdapter<String> aa = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, results);
+            listView.setAdapter(aa);*/
+
+         FoodItemExtended tempFood = recipeItems.get(recipeItems.size() - 1);
+            Toast.makeText(getApplicationContext(),
+                    "Added " + tempFood.grams + " " + tempFood.units + " of " + tempFood.description + " worth " + tempFood.calories + " calories", Toast.LENGTH_SHORT).show();
+
+
+             /*Toast.makeText(getApplicationContext(),
+                     "Items in recipe: " + recipeItems.size(), Toast.LENGTH_SHORT).show();*/
+
+
+
+        }
+    }
+
+
+    //Superclass to hold food items, used to parse search results
+    private class FoodItem {
         String description = "";
-        String brand = "";
         int id;
 
-        public foodItem(String descriptionReceived, String brandReceived, int idReceived) {
+        public FoodItem(){
+            description = "";
+            id = 0;
+        }
+
+        public FoodItem(String descriptionReceived, int idReceived) {
             description = descriptionReceived;
-            brand = brandReceived;
             id = idReceived;
         }
+    }
+
+    //subclass to pass selected results to the recipe
+    private class FoodItemExtended extends FoodItem {
+        int calories;
+        int grams;
+        String units;
+
+        public FoodItemExtended(){
+            calories = 0;
+            grams = 0;
+            units = "";
+        }
+
+        public FoodItemExtended(FoodItem foodItem, int cals, int gs, String us){
+            description = foodItem.description;
+            id = foodItem.id;
+            calories = cals;
+            grams = gs;
+            units = us;
+        }
+
+        public FoodItemExtended(FoodItem foodItem){
+            description = foodItem.description;
+            id = foodItem.id;
+            calories = 0;
+            grams = 0;
+            units = "";
+        }
+
     }
 
 }
